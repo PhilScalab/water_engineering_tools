c1cb39b63a781b93048aa31d71e2621381d72482
PhilScalab <75718170+PhilScalab@users.noreply.github.com>
2023-04-10 15:31:34 -0400

    Add files via upload

--- water_engineering_tools.py
+++ water_engineering_tools.py
@@ -294,6 +294,8 @@ if choice == "Hydrograph Producer":
                 wb, "hydrograph_analysis.xlsx"), unsafe_allow_html=True)
 
         # Convert the "Date" column to a datetime object
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
         df["Date"] = pd.to_datetime(df["Date"])
         years = df["Year"].unique()
         st.subheader("Hydrographs")
