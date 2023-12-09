5157b460ca46e8a7c4ca2c0096a7ecb4ccadd837
PhilScalab <75718170+PhilScalab@users.noreply.github.com>
2023-12-09 14:49:53 -0500

    Update water_engineering_tools.py

--- water_engineering_tools.py
+++ water_engineering_tools.py
@@ -624,7 +624,7 @@ if choice == "Ice Analysis":
         plt.tight_layout()
         st.pyplot(fig)
         # Dropdown for Stefan's coefficient
-        stefans_coefficient = st.selectbox('Stefan\'s Coefficient', np.arange(0.7, 3.1, 0.1))
+        stefans_coefficient = st.selectbox('Stefan\'s Coefficient', [0.7, 0.8, 0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0])
         
         # Dropdown for Effective Resistance of the Ice
         effective_resistance = st.selectbox('Effective Resistance of the Ice', [350, 400, 700, 1100, 1500])
